faf8ac7c05433d4c9006b1fa8f2fc29b8cbad0da
NaotoCoding <naoto.zaniah@gmail.com>
2024-08-02 22:26:20 +0900

    SeedでUserの初期データを作成できるようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+["First User", "Second User", "Third User"].each do |nickname|
+  User.create!(nickname:)
+end
