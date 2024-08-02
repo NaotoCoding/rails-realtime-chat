9184a71d8af3cf278d71ae10dbccc8a90c668d6d
NaotoCoding <naoto.zaniah@gmail.com>
2024-08-02 22:14:39 +0900

    generateコマンドで不要なファイルを生成しないようにした

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,9 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  
+  config.generators do |g|
+    g.assets false
+    g.test_framework false
+  end
 end
