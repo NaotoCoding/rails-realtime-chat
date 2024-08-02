7a82e7df70aa0607ccae556f45c9e46d652454f3
NaotoCoding <naoto.zaniah@gmail.com>
2024-08-02 22:33:36 +0900

    chat_roomsテーブルを作成

---
+++ app/models/chat_room.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: chat_rooms
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_chat_rooms_on_name  (name) UNIQUE
+#
+class ChatRoom < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_02_131805) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_02_132809) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chat_rooms", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_chat_rooms_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nickname", null: false
