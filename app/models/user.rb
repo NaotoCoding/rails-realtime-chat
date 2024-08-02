2968cdad1eba2207ddfd9aaf7c69f49ba3b109a6
NaotoCoding <naoto.zaniah@gmail.com>
2024-08-02 22:21:06 +0900

    annotateを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  nickname   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_nickname  (nickname) UNIQUE
+#
 class User < ApplicationRecord
   # あくまでチャットアプリのサンプルなのでログイン機構は省略
   validates :name, presence: true, uniqueness: true
