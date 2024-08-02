251befc760f91f4c6f761f41613609d50a1d8dc0
NaotoCoding <naoto.zaniah@gmail.com>
2024-08-02 23:25:48 +0900

    メッセージ一覧表示機能を作成

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -4,4 +4,9 @@ class ChatRoomsController < ApplicationController
   def index
     @chat_rooms = current_user.chat_rooms
   end
+
+  def show
+    @chat_room = current_user.chat_rooms.find(params[:id])
+    @chat_messages = @chat_room.chat_messages
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root to: "chat_rooms#index"
-  resources :chat_rooms, only: [:index]
+  resources :chat_rooms, only: [:index, :show]
 
   get "users/sign_in" => "sessions#new"
   post "users/sign_in" => "sessions#create"
